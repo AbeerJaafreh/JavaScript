57229af7df381dffbc1bfe5b3eccc32a57d467b9
AbeerJaafreh <51743449+AbeerJaafreh@users.noreply.github.com>
2019-07-10 12:24:32 +0300

    Add files via upload

---
+++ quizes/July/quiz/expression.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: First Expression 
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// this expression equals 4, change it to equal 42
+var number1=12;
+var number2=8;
+var number3=2;
+var number4=6;
+console.log(number1*number2/number3-number4);
---
+++ quizes/July/quiz/favorite-food.js
@@ -0,0 +1,7 @@
+/*
+ * Programming Quiz: Favorite Food 
+ * Create a string with the name of your favorite food. 
+ The first letter of the string should be capitalized.
+ */
+var favFood="fish"
+console.log(favFood.charAt(0).toUpperCase()+favFood.slice(1));
